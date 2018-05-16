d069bcb7cbf4fa9bb315f0ab9d2311d458f8e7d5
markobojkic <marko.bojkic@uns.ac.rs>
2018-05-16 15:40:37 +0200

    Vežbe 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { VoziloComponent } from './primer-components/vozilo/vozilo.component';
-import { AutomobilComponent } from './primer-components/automobil/automobil.component';
+import { VoziloComponent } from './components/primer-components/vozilo/vozilo.component';
+import { AutomobilComponent } from './components/primer-components/automobil/automobil.component';
 import {
   MatButtonModule,
   MatIconModule,
@@ -19,15 +19,28 @@ import {
   MatOptionModule
 } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ArtiklComponent } from './artikl/artikl.component';
-import { DobavljacComponent } from './dobavljac/dobavljac.component';
-import { PorudzbinaComponent } from './porudzbina/porudzbina.component';
-import { StavkaPorudzbineComponent } from './stavka-porudzbine/stavka-porudzbine.component';
-import { AboutComponent } from './core/about/about.component';
-import { AuthorComponent } from './core/author/author.component';
-import { HomeComponent } from './core/home/home.component';
+import { ArtiklComponent } from './components/artikl/artikl.component';
+import { DobavljacComponent } from './components/dobavljac/dobavljac.component';
+import { PorudzbinaComponent } from './components/porudzbina/porudzbina.component';
+import { StavkaPorudzbineComponent } from './components/stavka-porudzbine/stavka-porudzbine.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { AuthorComponent } from './components/core/author/author.component';
+import { HomeComponent } from './components/core/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { ArtiklService } from './services/artikl.service';
+
+
+const Routes = [
+  { path: 'artikl', component: ArtiklComponent },
+  { path: 'dobavljac', component: DobavljacComponent },
+  { path: 'porudzbina', component: PorudzbinaComponent },
+  { path: 'stavkaPorudzbine', component: StavkaPorudzbineComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'author', component: AuthorComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -48,9 +61,11 @@ import { HttpClientModule } from '@angular/common/http';
     MatButtonModule, MatIconModule, MatSidenavModule, MatListModule,
     MatGridListModule, MatExpansionModule, MatSortModule,
     MatTableModule,
-    MatToolbarModule, MatSelectModule, MatOptionModule
+    MatToolbarModule, MatSelectModule, MatOptionModule,
+    RouterModule.forRoot(Routes),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ArtiklService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/artikl/artikl.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ArtiklService } from '../../services/artikl.service';
+import { Observable } from 'rxjs/Observable';
+import { Artikl } from '../../models/artikl';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-artikl',
+  templateUrl: './artikl.component.html',
+  styleUrls: ['./artikl.component.css']
+})
+export class ArtiklComponent implements OnInit {
+
+  displayedColumns = ['id', 'naziv', 'proizvodjac', 'actions'];
+  exampleDatabase: ArtiklService;
+  dataSource: Observable<Artikl[]>;
+  index: number;
+  id: number;
+
+  constructor(public httpClient: HttpClient, public artiklService: ArtiklService) { }
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  public loadData() {
+    this.exampleDatabase = new ArtiklService(this.httpClient);
+    this.dataSource = this.artiklService.getAllArtikl();
+  }
+
+}
